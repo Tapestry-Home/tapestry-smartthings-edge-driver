3608bac13cee48af9f27921edc4bcafeea080b60
Elaine <pyoon99@gmail.com>
2024-05-01 19:47:54 -0700

    tweaking with changes suggested from community

--- src/init.lua
+++ src/init.lua
@@ -3,21 +3,11 @@ local zcl_clusters = require "st.zigbee.zcl.clusters"
 local ZigbeeDriver = require "st.zigbee"
 local constants = require "st.zigbee.constants"
 local defaults = require "st.zigbee.defaults"
-local contact_sensor_defaults = require "st.zigbee.defaults.contactSensor_defaults"
 local data_types = require "st.zigbee.data_types"
-local common = require("common")
 
--- local function added(driver, device) 
---     --Add the manufacturer-specific attributes to generate their configure reporting and bind requests
---     for capability_id, configs in pairs(common.get_cluster_configurations(device:get_manufacturer())) do
---         if device:supports_capability_by_id(capability_id) then
---             for _, config in pairs(configs) do
---                 device:add_configured_attribute(config)
---                 device:add_monitored_attribute(config)
---             end
---         end
---     end
--- end
+local function added(driver, device) 
+    device:refresh()
+end
 
 ----------------------Driver configuration----------------------
 local handlers = {
@@ -35,7 +25,7 @@ local thpz1_driver_template = {
     },
     zigbee_handlers = handlers,
     lifecycle_handlers = {
-        -- added = added
+        added = added
     },
 }
 
